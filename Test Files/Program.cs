935a023dadb06915fae1c8296a73c7ab71208f6b
Tim Koopman <tkoopman@users.noreply.github.com>
2025-03-07 10:38:26 +1000

    Add project files.

---
+++ Test Files/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Safe test executable file has run!");
+Console.WriteLine("Press any key to close...");
+Console.ReadKey();
