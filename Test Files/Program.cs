862bf5c21e5c67e511bc26f3dfcdc17ac7a4357b
Tim Koopman <tkoopman@users.noreply.github.com>
2025-03-07 10:59:09 +1000

    v1

--- Test Files/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Safe test executable file has run!");
-Console.WriteLine("Press any key to close...");
-Console.ReadKey();
